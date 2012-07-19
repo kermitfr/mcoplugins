e6eede03f5c7d580bd6fa6bda78c385e36eb36c1
Marco Mornati <ilmorna@gmail.com>
2012-07-19 15:25:03 +0200

    Added PuppetMaster Agent with Hiera Control

---
+++ puppetmaster.ddl
@@ -0,0 +1,15 @@
+metadata    :name => "PuppetMaster",
+            :description => "PuppetMaster Operations",
+            :author => "Marco Mornati",
+            :license => "Apache License 2.0",
+            :version => "0.1",
+            :url => "http://www.kermit.fr/",
+            :timeout => 120 
+
+action "classlist", :description => "Get Puppet Classes List" do
+
+    output :classlist,
+           :description => "Defined Classlist",
+           :display_as => "Classlist"
+end
+
---
+++ puppetmaster.rb
@@ -0,0 +1,29 @@
+module MCollective
+    module Agent
+        class Puppetmaster<RPC::Agent
+            metadata    :name => "PuppetMaster",
+                        :description => "Puppet Master Operations",
+                        :author => "Marco Mornati",
+                        :license => "Apache License 2.0",
+                        :version => "0.1",
+                        :url => "http://www.kermit.fr/",
+                        :timeout => 120
+
+	    action "classlist" do
+		class_list
+	    end
+
+            private
+
+	    def class_list
+		require 'puppet'
+		require 'puppet/face'
+		require 'pp'
+
+		classlist = Puppet::Face[:resource_type,:current].search('.*').find_all {|x| x.type==:hostclass}.collect{|x| x.name}.sort
+
+		reply[:classlist] = classlist
+	    end
+        end
+    end
+end
