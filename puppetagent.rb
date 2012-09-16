bb5baa9f651be8c8ee1e0a992c638e67c96ac31f
lofic <louis.coilliot@gmail.com>
2012-09-16 19:18:39 +0200

    Exception on classlist for nodes that are not puppetmasters

--- puppetagent.rb
+++ puppetagent.rb
@@ -3,23 +3,23 @@ module MCollective
         class Puppetagent<RPC::Agent
             metadata    :name => "Puppet Module",
                         :description => "Puppet Operations",
-                        :author => "Marco Mornati",
+                        :author => "Marco Mornati, Louis Coilliot",
                         :license => "Apache License 2.0",
-                        :version => "0.1",
+                        :version => "0.2",
                         :url => "http://www.kermit.fr/",
                         :timeout => 120
 
-	    action "classlist" do
-		class_list
-	    end
+        action "classlist" do
+          class_list
+        end
 
-	    action "forceupdate" do
-		force_update
-	    end
+        action "forceupdate" do
+          force_update
+        end
 
-            private
+        private
 
-	    def force_update
+        def force_update
                 puppet = "/usr/bin/puppet"
                 cmd = [puppet, "agent", "--color no", "--onetime", "--verbose",
                 "--ignorecache", "--no-daemonize" , "--no-usecacheonfailure",
@@ -29,17 +29,22 @@ module MCollective
                 end
                 cmd = cmd.join(" ")
                 reply[:output] = %x[#{cmd}].to_a.last.chomp
-            end
+        end
 
-	    def class_list
-		require 'puppet'
-		require 'puppet/face'
-		require 'pp'
+        def class_list
+          require 'puppet'
+          require 'puppet/face'
 
-		classlist = Puppet::Face[:resource_type,:current].search('.*').find_all {|x| x.type==:hostclass}.collect{|x| x.name}.sort
+          begin
+              # Works only on a puppet master
+              classlist = Puppet::Face[:resource_type,:current].search('.*').find_all {|x| x.type==:hostclass}.collect{|x| x.name}.sort
+          rescue NoMethodError
+              classlist = []
+          end
 
-		reply[:classlist] = classlist
-	    end
+          reply[:classlist] = classlist
+        end
+
         end
     end
 end
