d5e3f936fa11c30d748ca6975f281c34b9199580
lofic <louis.coilliot@gmail.com>
2013-06-06 15:30:31 +0200

    Agent renamed

--- puppetmaster/puppetmaster.ddl
+++ puppetmaster/puppetmaster.ddl
@@ -1,5 +1,5 @@
 metadata    :name => "Puppet Module",
-            :description => "Puppet Operations",
+            :description => "Puppet Master Operations",
             :author => "Marco Mornati",
             :license => "Apache License 2.0",
             :version => "0.1",
@@ -11,12 +11,5 @@ action "classlist", :description => "Get Puppet Classes List" do
     output :classlist,
            :description => "Defined Classlist",
            :display_as => "Classlist"
-end
-
-action "forceupdate", :description => "Force puppet agent update" do
-
-    output :result,
-           :description => "Update result",
-           :display_as => "Result"
 end
 
--- puppetmaster/puppetmaster.rb
+++ puppetmaster/puppetmaster.rb
@@ -1,28 +1,12 @@
 module MCollective
     module Agent
-        class Puppetagent<RPC::Agent
+        class Puppetmaster<RPC::Agent
 
         action "classlist" do
           class_list
         end
 
-        action "forceupdate" do
-          force_update
-        end
-
         private
-
-        def force_update
-            puppet = "/usr/bin/puppet"
-            cmd = [puppet, "agent", "--color no", "--onetime", "--verbose",
-            "--ignorecache", "--no-daemonize" , "--no-usecacheonfailure",
-            "--no-splay" ]
-            if request[:server]
-                cmd << "--server #{request[:server]}"
-            end
-            cmd = cmd.join(" ")
-            reply[:output] = %x[#{cmd}].to_a.last.chomp
-        end
 
         def class_list
           require 'puppet'
@@ -31,10 +15,10 @@ module MCollective
           begin
               # for puppet 3 :
               ::Puppet.settings.initialize_global_settings(['--confdir=/etc/puppet'])
-	  rescue ::Puppet::DevError
-	      # Already initialized
-	  end
-	  begin
+          rescue ::Puppet::DevError
+              # Already initialized
+          end
+          begin
               # Works only on a puppet master
               classlist = ::Puppet::Face[:resource_type,:current].search('.*').find_all {|x| x.type==:hostclass}.collect{|x| x.name}.sort
           rescue RuntimeError, NoMethodError
